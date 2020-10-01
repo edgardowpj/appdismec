b9796612757b43cd8c3cb9281da68a29690c863c
MichaelNegrete16 <60947644+MichaelNegrete16@users.noreply.github.com>
2020-10-01 05:29:51 -0500

    Suvi
    Avance del proyecto

---
+++ app/src/main/java/com/example/practi/Login.java
@@ -0,0 +1,108 @@
+package com.example.practi;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.material.textfield.TextInputEditText;
+import com.vishnusivadas.advanced_httpurlconnection.PutData;
+
+public class Login extends AppCompatActivity {
+
+    TextInputEditText textInputEditTextUsername, textInputEditTextPassword;
+    Button buttonLogin;
+    TextView textViewLogin,textViewLogin2;
+    ProgressBar progressBar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        textInputEditTextUsername=findViewById(R.id.usernameLogin);
+        textInputEditTextPassword=findViewById(R.id.passwordLogin);
+        buttonLogin=findViewById(R.id.btnLogin);
+        progressBar=findViewById(R.id.progress);
+        textViewLogin=findViewById(R.id.loginText2);
+        textViewLogin2=findViewById(R.id.loginText3);
+
+        textViewLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), SignUp.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+        textViewLogin2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), SignUpProfesor.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+        buttonLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                final String fullname,username,password,email;
+                username= String.valueOf(textInputEditTextUsername.getText());
+                password= String.valueOf(textInputEditTextPassword.getText());
+
+                if(!username.equals("") && !password.equals("")){
+
+                    //Start ProgressBar first (Set visibility VISIBLE)
+                    progressBar.setVisibility(View.VISIBLE);
+                    Handler handler = new Handler(Looper.getMainLooper());
+                    handler.post(new Runnable() {
+                        @Override
+                        public void run() {
+                            //Starting Write and Read data with URL
+                            //Creating array for parameters
+                            String[] field = new String[2];
+                            field[0] = "username";
+                            field[1] = "password";
+                            String[] data = new String[2];
+                            data[0] = username;
+                            data[1] = password;
+                            PutData putData = new PutData("http://192.168.1.55/proyecto/login.php", "POST", field, data);
+
+                            PutData putData2 = new PutData("http://192.168.1.55/proyecto/loginProfesor.php", "POST", field, data);
+                            if (putData.startPut()) {
+                                if (putData.onComplete()) {
+                                    progressBar.setVisibility(View.GONE);
+                                    String result = putData.getResult();
+                                    String result2 = putData2.getResult();
+                                    if(result.equals("Login Success")){
+                                        Toast.makeText(getApplicationContext(),result,Toast.LENGTH_SHORT).show();
+                                        Intent intent = new Intent(getApplicationContext(), MainActivity2.class);
+                                        startActivity(intent);
+                                        finish();
+                                    }else{
+                                        Toast.makeText(getApplicationContext(),result,Toast.LENGTH_SHORT).show();
+                                    }
+
+                                }
+                            }
+
+                        }
+                    });
+                }else{
+                    Toast.makeText(getApplicationContext(),"All fields required",Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+}
